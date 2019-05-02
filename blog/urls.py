a3113cf142a36611961bebe98ca524fb67364ce5
SaiKrishna <saikrishnasv0@gmail.com>
2019-05-03 01:10:44 +0530

    Edit and add new blog features added to the app

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('postlist/', views.post_list, name='post_list'),
-    path('postdetail/<int:id>/',views.post_detail, name='post_detail')
+    path('postdetail/<int:id>/',views.post_detail, name='post_detail'),
+    path('post/new/',views.post_new,name='post_new'),
+    path('postedit/<int:id>/',views.post_edit,name='post_edit'),
 ]
