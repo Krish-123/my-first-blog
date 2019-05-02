f68380401637c958b24a83ca78adef4b3748a73a
SaiKrishna <saikrishnasv0@gmail.com>
2019-05-02 23:08:31 +0530

    Blog detail page added with variable url

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('postlist/', views.post_list, name='post_list'),
+    path('postdetail/<int:id>/',views.post_detail, name='post_detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -9,3 +9,10 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 
     return render(request,'blog/post_list.html',{'posts':posts})
+
+
+def post_detail(request,id):
+    published_posts = Post.objects.filter(published_date__lte=timezone.now())
+    post = get_object_or_404(published_posts,pk=id)
+
+    return render(request,'blog/post_detail.html',{'post':post})
